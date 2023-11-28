4a418cc70deed3e367032a124d6b1ab2cb1c768d
Ezequiel <ki3lmigu3l@gmail.com>
2023-11-27 21:22:46 -0300

    Estrutura do nosso backend

---
+++ todo-list/src/main/java/com/github/ki3lmigu3l/todolist/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.github.ki3lmigu3l.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
---
+++ todo-list/src/test/java/com/github/ki3lmigu3l/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ki3lmigu3l.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
